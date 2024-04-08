ff22e1149a903da7620f485ec93045361ac56eb3
shashank8112357010 <shashanksharma1235999@gmail.com>
2024-04-08 11:49:48 +0530

    optimized footer

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -2,73 +2,71 @@ import Image from "next/image"
 
 
 export default function Footer() {
+
+    const footerLinks = [
+        {
+            "heading": "Category",
+            "data": ["Brand & Identity", "Business & Advertising", "Packaging & Label Design", "Website & App Design", "Social Media Design", "Illustration & Art", "Book & Magzine Design", "Clothing & Merchandise"]
+        },
+        {
+            "heading": "Get design",
+            "data": ["Logo design", "Business cards", "Product packaging", "Landing page design", "website development", "Social media profile banners", "Custom illustrations"]
+        },
+        {
+            "heading": "Support",
+            "data": ["Help Desk", "Disclaimer", "Privacy Policy", "Terms  and Conditions"]
+        },
+        {
+            "heading": "Company",
+            "data": ["About us ", "Pricing", "About us "]
+        },
+        {
+            "heading": "Resources",
+            "data": ["Documentation", "Papers", "Press Conferences"]
+        }
+    ]
+    const socialLinks = [
+        {
+            img: "Facebook",
+        },
+        {
+            img: "Insta",
+        },
+        {
+            img: "Twitter",
+        },
+        {
+            img: "Lknd",
+        },
+        {
+            img: "Pint",
+        }
+    ]
     return (
         <section className="max-w-[1920px] mx-auto bg-primaryBlack text-white  sm:px-8">
-
-
             <div className="flex justify-center flex-col sm:flex-row lg:flex-row  text-center sm:text-start gap-[33px]  sm:text-normal sm:justify-normal  flex-wrap lg:pl-10 relative  ">
                 {/* <div className="flex flex-col sm:flex-row lg:flex-row"> */}
-                <div className="flex-col flex gap-4 sm:w-[200px] lg:w-[190px]">
-                    <p className=" text-mobfooterTitle sm:text-tabfooterTitle lg:text-lapfooterTitle font-bold">Category</p>
-                    <ul className="text-mobfooterLinks sm:text-tabfooterLinks lg:text-lapfooterLinks leading-8 font-normal text-primaryGrey">
-                        <li className="cursor-pointer">Brand & Identity</li>
-                        <li className="cursor-pointer">Business & Advertising</li>
-                        <li className="cursor-pointer">Packaging & Label Design</li>
-                        <li className="cursor-pointer">Website & App Design</li>
-                        <li className="cursor-pointer">Social Media Design</li>
-                        <li className="cursor-pointer">Illustration & Art</li>
-                        <li className="cursor-pointer">Book & Magzine Design</li>
-                        <li className="cursor-pointer">Clothing & Merchandise</li>
-                    </ul>
-                </div>
-                <div className="flex-col flex gap-4 sm:w-[210px] lg:w-[180px]">
-                    <p className="text-mobfooterTitle sm:text-tabfooterTitle lg:text-lapfooterTitle font-bold">Get design</p>
-                    <ul className="text-mobfooterLinks sm:text-tabfooterLinks lg:text-lapfooterLinks font-normal text-primaryGrey leading-8">
-                        <li className="cursor-pointer">Logo design</li>
-                        <li className="cursor-pointer">Business cards</li>
-                        <li className="cursor-pointer">Product packaging</li>
-                        <li className="cursor-pointer">Landing page design</li>
-                        <li className="cursor-pointer">website development</li>
-                        <li className="cursor-pointer">Business cards</li>
-                        <li className="cursor-pointer">Social media profile banners</li>
-                        <li className="cursor-pointer">Custom illustrations</li>
-                    </ul>
-                </div>
-                {/* </div> */}
-                {/* <div className="flex gap-2   lg:flex-row flex-col"> */}
-                <div className="flex-col flex gap-4 sm:w-[200px] lg:w-[100px]">
-                    <p className="text-mobfooterTitle sm:text-tabfooterTitle lg:text-lapfooterTitle font-bold">Support</p>
-                    <ul className="text-mobfooterLinks sm:text-tabfooterLinks lg:text-lapfooterLinks font-normal text-primaryGrey leading-8">
-                        <li className="cursor-pointer">Help Desk</li>
-                        <li className="cursor-pointer">Disclaimer</li>
-                        <li className="cursor-pointer">Privacy Policy</li>
-                        <li className="cursor-pointer">Terms  and Conditions</li>
-                    </ul>
-                </div>
-                <div className="flex-col flex gap-4 sm:w-[200px] lg:w-[120px]">
-                    <p className="text-mobfooterTitle sm:text-tabfooterTitle lg:text-lapfooterTitle font-bold">Company</p>
-                    <ul className="text-mobfooterLinks sm:text-tabfooterLinks lg:text-lapfooterLinks font-normal text-primaryGrey leading-8">
-                        <li className="cursor-pointer">About us</li>
-                        <li className="cursor-pointer">Pricing</li>
-                        <li className="cursor-pointer">Contact Us</li>
-                    </ul>
-                </div>
-                <div className="flex-col flex gap-4 lg:w-[100px] ">
-                    <p className="text-mobfooterTitle sm:text-tabfooterTitle lg:text-lapfooterTitle font-bold">Resources</p>
-                    <ul className="text-mobfooterLinks sm:text-tabfooterLinks lg:text-lapfooterLinks font-normal text-primaryGrey leading-8">
-                        <li className="cursor-pointer">Documentation</li>
-                        <li className="cursor-pointer">Papers</li>
-                        <li className="cursor-pointer">Press Conferences</li>
-                    </ul>
-                </div>
-                {/* </div> */}
+                {
+                    footerLinks && footerLinks.map((footer , index) => {
+                        return (
+                            <div key={index} className={`flex-col flex gap-4  ${footer.heading === "Category" ? 'sm:w-[200px] lg:w-[190px]' : footer.heading === "Get design" ? 'sm:w-[210px] lg:w-[180px]' : footer.heading === "Support" ? 'sm:w-[200px] lg:w-[100px]' : footer.heading === "Company" ? 'sm:w-[200px] lg:w-[120px]' : "lg:w-[100px]"}   `}
+                            >
+                                <p className=" text-mobfooterTitle sm:text-tabfooterTitle lg:text-lapfooterTitle font-bold">{footer.heading}</p>
+                                <ul className="text-mobfooterLinks sm:text-tabfooterLinks  lg:text-lapfooterLinks leading-8 font-normal text-primaryGrey"> {footer.data.map((item , indexx) => <li className="cursor-pointer" key={indexx}>{item}</li>)}
+                                </ul>
+                            </div>
+                        )
+                    })
+                }
                 <div className="flex-col  lg:flex gap-6 sm:block w-[250px] mx-auto sm:w-[338px] lg:w-[250px]  ">
                     <Image src="/navbar/Logo.png" width={230} height={54} alt="logoImg" className="" />
-                    <p className="text-mobfooterBody sm:text-tabfooterBody lg:text-lapfooterBody font-normal leading-5">Logo Design Maker isn&#39;t just about creating logos – we build brands. We offer a comprehensive suite of design services to cater to all your branding needs, from logo design and business cards to website development and social media graphics.</p>
+                    <p className="text-mobfooterBody sm:text-tabfooterBody lg:text-lapfooterBody font-normal leading-5">
+                        {`Logo Design Maker isn&#39;t just about creating logos – we build brands. We offer a comprehensive suite of design services to cater to all your branding needs, from logo design and business  cards to website development and social media graphics.`}
+                    </p>
                 </div>
-                <div className="absolute bottom-[-10px] sm:bottom-[0px]  ">
-                   
-                    <Image  src="/footer/FooterGradient.png" width={1440} height={56} alt="FooterGradient" />
+                <div className="absolute bottom-[-10px] sm:bottom-[0px]">
+
+                    <Image src="/footer/FooterGradient.png" width={1440} height={56} alt="FooterGradient" />
                 </div>
             </div>
             <div className="flex sm:justify-between justify-center items-center gap-[24px] sm:flex-row flex-col w-full mx-auto px-4 lg:px-[80px] pt-[40px] pb-[20px] ">
@@ -76,14 +74,15 @@ export default function Footer() {
                     <p className="text-lapTitle3 whitespace-nowrap">© 2024 LogoDesignMaker Inc. All rights reserved.</p>
                 </div>
                 <div className="flex justify-between gap-[32px]">
-                    <Image src="/footer/Facebook.png" width={18} height={18} alt="FacebookImg" />
-                    <Image src="/footer/Insta.png" width={18} height={18} alt="InstagramImg" />
-                    <Image src="/footer/Twitter.png" width={18} height={18} alt="TwitterImg" />
-                    <Image src="/footer/Lknd.png" width={18} height={18} alt="LinkedinImg" />
-                    <Image src="/footer/Pint.png" width={18} height={18} alt="PintrestImg" />
+                    {
+                        socialLinks.map((links , index) => {
+                            return (
+                                <Image key={index} src={`/footer/${links.img}.png`} width={18} height={18} alt={`${links.img}Img`} />
+                            )
+                        })
+                    }
                 </div>
             </div>
-
         </section>
     )
 
