9efd861ccbfa2e08a49002a42d43637c996f4801
28varun <107782101+28varun@users.noreply.github.com>
2024-04-10 17:22:32 +0530

    dropdown 50%

--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -5,10 +5,64 @@ import { useState } from "react"
 export default function Navbar() {
     const [hide, setHide] = useState(false)
     const [show, setShow] = useState(false)
+    const dropMenu = [
+        {
+            id:1,
+           img:"/navbar/BookIcon.png",
+           heading:"Book and magazine designs",
+           subHeading:"Your Digital Content Supercharged" 
+        },
+        {
+            id:2,
+           img:"/navbar/BookIcon.png",
+           heading:"Business and advertising designs",
+           subHeading:"Eye Catchy Designs That Perform" 
+        },
+        {
+            id:3,
+           img:"/navbar/BookIcon.png",
+           heading:"Business and advertising designs",
+           subHeading:"Eye Catchy Designs That Perform" 
+        },
+        {
+            id:4,
+           img:"/navbar/BookIcon.png",
+           heading:"Business and advertising designs",
+           subHeading:"Eye Catchy Designs That Perform" 
+        },
+       
+    ]
+    const dropMenus = [
+        {
+            id:1,
+           img:"/navbar/BookIcon.png",
+           heading:"Book and magazine designs",
+           subHeading:"Your Digital Content Supercharged" 
+        },
+        {
+            id:2,
+           img:"/navbar/BookIcon.png",
+           heading:"Book and magazine designs",
+           subHeading:"Your Digital Content Supercharged" 
+        },
+        {
+            id:3,
+           img:"/navbar/BookIcon.png",
+           heading:"Book and magazine designs",
+           subHeading:"Your Digital Content Supercharged" 
+        },
+        {
+            id:4,
+           img:"/navbar/BookIcon.png",
+           heading:"Book and magazine designs",
+           subHeading:"Your Digital Content Supercharged" 
+        },
+       
+    ]
 
     return (
         <section className="relative max-w-[1920px] z-50 mx-auto bg-secondaryBlack h-[84px] text-white ">
-            <div className="flex justify-between items-center h-full px-4 sm:px-10 lg:px-20">
+            <div className="flex justify-between items-center h-full px-4 sm:px-10 lg:px-20 relative">
                 <div className="lg:hidden cursor-pointer" onClick={() => setHide(!hide)}>
                     {hide ?
                         <Image src="/navbar/Cross.png" width={26} height={10} alt="logoImg" />
@@ -29,12 +83,15 @@ export default function Navbar() {
                                 About Us
                             </a>
                         </li>
-                        <li className="flex gap-1 justify-center">
+                        <li className=" gap-1 flex flex-col justify-center relative">
+                            <div className="flex justify-center gap-1">
+
                             <a className="hover:text-primaryGreen" href="#">
                                 Services
                             </a>
                             <Image src="/navbar/expand_icon.svg" className="cursor-pointer" width={10} height={10} alt="logoImg" />
-                           
+                            </div>
+                     
                         </li>
                         <li><a className="hover:text-primaryGreen" href="#">Price</a></li>
                         <li><a className="hover:text-primaryGreen" href="#">Contact</a></li>
@@ -42,10 +99,69 @@ export default function Navbar() {
                     <button className="text-[10px] sm:text-[14px] font-semibold border-2 border-primaryGreen hover:bg-primaryGreen text-white hover:text-black px-[16px] sm:px-[28px] py-[6px] sm:py-[8px] rounded-[4px] transition-all duration-300 ease-in-out">Sign in</button>
                     <button className="hidden sm:block text-[10px] sm:text-[14px] font-semibold bg-primaryGreen border-2 border-primaryGreen hover:bg-secondaryBlack hover:border-primaryGreen text-black hover:text-white px-[14px] py-[8px] rounded-[4px] transition-all duration-300 ease-in-out">Get Started</button>
                 </div>
+                <div className="bg-darkBlack w-[910px] flex justify-between  absolute top-[112px] right-[154px]">
+                    
+                    <div className="flex flex-col justify-center gap-6 pl-[20px] py-10"> 
+                    <div>
+                       <p className="text-[16px] font-normal leading-[28px] tracking-[6%]">SERVICES</p> 
+                    </div>
+                    <div className="flex  justify-center gap-[39px]">
+                        <div className="flex flex-col items-start">
+                        {
+                            dropMenu.map((item)=>{
+                                return(
+                                    <div key={item.id} className="flex justify-center items-start gap-[6px] " >
+
+                                    <Image width={15} height={15} src={item.img} alt="BookIconImg"/>
+                                   <div>
+                                    <p className="text-lapTitle3 leading-[19px] font-bold">{item.heading}</p>
+                                    <p className="text-lapfooterBody text-primaryGrey  font-normal leading-[21px]">{item.subHeading}</p> 
+                                   </div>
+                                    </div> 
+                                )
+                            })
+                        }
+                            
+                        </div>
+                        <div className="flex flex-col">
+                        {
+                            dropMenus.map((item)=>{
+                                return(
+                                    <div key={item.id} className="flex justify-center items-start gap-[6px] " >
+
+                                    <Image width={15} height={15} src={item.img} alt="BookIconImg"/>
+                                   <div>
+                                    <p className="text-lapTitle3 leading-[19px] font-bold">{item.heading}</p>
+                                    <p className="text-lapfooterBody text-primaryGrey  font-normal leading-[21px]">{item.subHeading}</p> 
+                                   </div>
+                                    </div> 
+                                )
+                            })
+                        }
+                                    
+                        </div>
+                    </div>
+
+                    </div>
+                    <div className="flex flex-col justify-center items-center bg-lightBlack w-[236px]">
+                    <Image width={163} height={163} src='/navbar/DropDownMedia.png' alt="BookIconImg"/>
+                    <div className="w-[187px] flex flex-col  gap-2 pr-2 ">
+                        <p className="text-lapfooterBody leading-[21px] tracking-[6%] font-normal capitalize">BOOK A CALL </p>
+                        <p className="text-lapTitle3 font-bold leading-[19px] -tracking-[1%]">Want to know if Logo design maker is A good fit?</p>
+                    </div>
+                    <div className="flex relative items-center justify-center w-[200px] pb-2 px-4  " >
+                            <input type="text" placeholder="Enter Your Email" className="text-primaryBlack outline-none border-none rounded-[45px] inputDesign2 pt-[2px] pr-[5px] pb-[2px] pl-[10px] " />
+                            <div className="absolute right-[18px] top-[5px] sm:top-0">
+                                <button className="bg-buttonBlack text-[10px] leading-[14px] font-semibold rounded-[45px] py-[5px] px-[8px]">Start</button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 2pxtop, 5pxrigth, 2pxbottom, 10.01pxleft */}
             </div> 
-            {/* <div className="bg-red-700 flex justify-center pl-32 ">
-                    <p>varun</p>
-                </div>  */}
+
+
+            </div> 
+          
 
             <div className={`-z-40 bg-secondaryBlack flex flex-col lg:hidden absolute ${hide ? "left-[0px] top-20" : "-py-0 top-12 -left-[72rem]"} bg-lightWhite w-full transition-all duration-500 ease-in-out rounded-br-[20px] rounded-bl-[20px]`} >
                 <div className="bg-secondaryBlack absolute w-full">
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
       colors: {
         primaryBlack: "#0B1926",
         secondaryBlack: "#182736",
+        darkBlack: "#12202E",
+        lightBlack: "#1F3143",
         buttonBlack: "#2F2F2F",
         primaryGreen: "#5CFF85",
         primaryGray: "#BDBDBD",
