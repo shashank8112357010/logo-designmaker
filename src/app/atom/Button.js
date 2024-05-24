3483ace7aab1eb5af5e8a5489ee35cc58fe4ebfa
amanfrontdev <amanfrontdev@gmail.com>
2024-05-24 22:57:33 +0530

    code pushed

--- src/app/atom/Button.js
+++ src/app/atom/Button.js
@@ -1,7 +1,7 @@
+"use client"
 import { useRouter } from "next/navigation";
 
 export default function Button({ title, displayy, urlLink }) {
-
     const router = useRouter()
     return (
         <button onClick={() => router.push(urlLink)} className={`${displayy} text-[10px] sm:text-[14px] font-semibold bg-primaryGreen border-2 border-primaryGreen hover:bg-secondaryBlack hover:border-primaryGreen text-black hover:text-white px-[14px] py-[8px] rounded-[4px] transition-all duration-300 ease-in-out`} aria-disabled="true">
