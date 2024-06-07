fdf7d3490759f56300695c58a791366b5a874cbf
amanfrontdev <amanfrontdev@gmail.com>
2024-06-07 17:44:30 +0530

    added new page

--- src/data/merchandise-design.js
+++ src/data/merchandise-design.js
@@ -1,21 +1,24 @@
 
 export const servicesDesignData = [
-    { id: 1, heading: "Extensive Product Selection: ", subHeading: " We offer a wide variety of merchandise products to suit your brand and marketing goals. This includes t-shirts, hoodies, hats, bags, mugs, and more. " },
-    { id: 2, heading: "Custom Design & Branding: ", subHeading: " Our team of experienced designers will work with you to create custom design merchandise that reflects your brand identity and resonates with your target audience." },
-    { id: 3, heading: "High-Quality Printing & Materials: ", subHeading: " We use state-of-the-art printing technologies and premium materials to ensure your merchandise is not only visually stunning but also durable and long-lasting." },
+    { id: 1, heading: "Imaginative Designs: ", subHeading: " Our group has some expertise in making eye-getting designs that line up with your image's character." },
+    { id: 2, heading: "Great Products: ", subHeading: " We utilize premium materials to guarantee your merchandise looks proficient and lasts long." },
+    { id: 3, heading: "Quick Turnaround: ", subHeading: " We figure out the significance of cutoff times and endeavor to speedily convey your custom merchandise." },
+    { id: 4, heading: "Custom Solutions: ", subHeading: " Whether you really want a little cluster of things or an enormous request, our services are adaptable to address your issues." },
+    { id: 5, heading: "Thorough Branding: ", subHeading: " We guarantee that all merchandise designs are reliable with your general image technique, giving a strong look across all stages." },
+    { id: 6, heading: "Innovative Consultation: ", subHeading: " Our designers work intimately with your group to conceptualize and foster interesting thoughts that resound with your interest group." },
 ]
 
 export const faqs = [
-    { id: 1, ques: "What type of merchandise products do you offer?", ans: "We offer a wide range of high-quality merchandise tailored to your brand and marketing needs, including apparel, bags, drinkware, accessories, and stationery. Let us know your requirements, and we'll find the perfect product for your custom design." },
-    { id: 2, ques: "Can I upload my own artwork for my merchandise design?", ans: "The design process typically takes 2-3 weeks, depending on the complexity of your project and the number of revisions required. However, we strive to deliver high-quality results efficiently while maintaining open communication throughout the process." },
-    { id: 3, ques: "What is the minimum order quantity for custom merchandise?", ans: "Absolutely! We offer a variety of branding packages that combine logo design with other services such as business card design, website design, and social media graphics. Feel free to contact us to discuss a custom package tailored to your specific needs." },
-    { id: 4, ques: "How long does it take to receive my custom merchandise?", ans: "Getting started is easy! Simply visit our website and fill out a brief form outlining your brand and design preferences. A member of our team will then be in touch to discuss your project further and answer any questions you may have. " }
+    { id: 1, ques: "What sorts of merchandise can you design?", ans: " We offer an extensive variety of custom design merchandise including clothing, promotional things, extras, from there, the sky's the limit. In the event that you have a particular thing as a main priority, go ahead and examine it with our group." },
+    { id: 2, ques: "How long does the merchandise design process take?", ans: "The timetable for our <strong>merchandise design services</strong> differs depending upon the intricacy and amount of the things. Regularly, the cycle can take from half a month to a month." },
+    { id: 3, ques: "Might I at any point demand corrections to the merchandise designs?", ans: "Totally! We offer limitless corrections during the design stage to guarantee your <strong>promotional merchandise</strong> meets your definite particulars and fulfillment." },
+    { id: 4, ques: "What makes Logo Design Creator's merchandise design services unique?", ans: "Our emphasis on customized administration, great items, and cooperative design process separates us. Our accomplished <strong>merchandise designers</strong> are committed to rejuvenating your vision with inventiveness and accuracy. " }
 ]
 
 export const designProcess = [
-    { id: 0, heading: "Initial Consultation", para: "We offer a wide range of high-quality merchandise tailored to your brand and marketing needs, including apparel, bags, drinkware, accessories, and stationery. Let us know your requirements, and we'll find the perfect product for your custom design." },
-    { id: 1, heading: "Content & Style Analysis", para: "Our designers will develop several unique merchandise design concepts based on your input." },
-    { id: 2, heading: "Concept Development", para: "Our team will translate your vision into a professional merchandise design. We offer multiple revision rounds to ensure you're completely satisfied with the final product" },
-    { id: 3, heading: "Revisions & Refinement", para: "We work closely with you throughout the process, incorporating your feedback and refining the design until it exceeds your expectations." },
-    { id: 4, heading: "Final Delivery", para: "You'll receive your final merchandise design files in various formats optimized for different e-reader platforms." },
+    { id: 0, heading: "Initial Consultation", para: "Talk about your vision and prerequisites with our master group to begin your <strong>custom design merchandise</strong> venture. We dive profound into figuring out your image, goals, and the particular results you want." },
+    { id: 1, heading: "Concept Development", para: "Our <strong>merchandise designers</strong> will give starting design ideas to your audit and criticism. These ideas are created with scrupulousness, guaranteeing they line up with your image's character and objectives." },
+    { id: 2, heading: "Design Refinement", para: "Team up with us to refine and consummate the design, guaranteeing it lives up to your assumptions. We empower open correspondence and worth your contribution at each stage, considering changes in variety, style, and format." },
+    { id: 3, heading: "Production", para: "When the design is finished, we continue with top notch creation, it is amazing to guarantee everything about. We use the best materials and printing methods to ensure that your merchandise looks and feels premium." },
+    { id: 4, heading: "Delivery", para: "Accept your completed<strong> merchandise design</strong> items instantly, prepared to dazzle your crowd. We guarantee opportune conveyance with the goal that you can begin utilizing your custom merchandise straightaway." },
 ]
