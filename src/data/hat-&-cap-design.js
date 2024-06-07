0d83bc3303ab17b8cd447fc5472487fceae71a54
PrathamShamra1111 <sharmapratham223@gmail.com>
2024-06-07 17:43:17 +0530

    hat and cap

---
+++ src/data/hat-&-cap-design.js
@@ -0,0 +1,22 @@
+
+export const servicesDesignData = [
+    { id: 1, heading: " Innovative Design Concepts: ", subHeading: " We specialize in crafting eye-catching designs that align impeccably with your brand's character. " },
+    { id: 2, heading: "Premium Quality Materials: ", subHeading: " Our hats and caps are made with great materials to guarantee durability and solace." },
+    { id: 3, heading: "Fast and Proficient Turnaround:  ", subHeading: " We understand the importance of complying with time constraints and endeavor to quickly convey your custom hats." },
+    { id: 4, heading: "Custom Solutions:  ", subHeading: " Whether you really want a small batch or a large request, our administrations are adaptable to accommodate your prerequisites.." },
+]
+
+export const faqs = [
+    { id: 1, ques: "What kinds of hats and caps can you design?", ans: "We offer an extensive variety of custom hat and cap design choices including baseball caps, snapbacks, beanies, and more. Go ahead and examine your particular requirements with our team." },
+    { id: 2, ques: "How long does the hat and cap design process take?", ans: "The course of events for our hat and cap design services varies relying upon the intricacy and quantity of the request. Typically, the interaction takes half a month from initial consultation to final conveyance." },
+    { id: 3, ques: "Can I demand modifications to the hat designs?", ans: "Absolutely! We offer limitless amendments during the design phase to guarantee your custom hats and caps meet your exact specifications and satisfaction." },
+    { id: 4, ques: " What makes Logo Design Maker's hat and cap design administrations unique?", ans: "Our emphasis on personalized help, top notch materials, and a collaborative design process separates us. Our accomplished hat designers are dedicated to rejuvenating your vision with creativity and accuracy." }
+]
+
+export const designProcess = [
+    { id: 0, heading: "Initial Consultation", para: "Examine your vision and prerequisites with our master team to kickstart your <strong>custom hat and cap design</strong> venture. We gather all necessary details about your brand, targets, and wanted results." },
+    { id: 1, heading: "Concept Development", para: " Our <strong>hat designers</strong> will give initial design ideas to your audit and feedback. These ideas are crafted with careful attention to detail to align with your brand's personality." },
+    { id: 2, heading: "Design Refinement", para: "Collaborate with us to refine and consummate the design, guaranteeing it measures up to your assumptions. We value your feedback and make adjustments based on your feedback on tones, styles, and layouts." },
+    { id: 3, heading: "Production", para: "When the design is finalized, we continue with great creation, guaranteeing everything executed flawlessly. Our obligation to quality means your custom hats and caps will look and feel premium." },
+    { id: 4, heading: "Delivery", para: "Accept your completed <strong>custom hats and caps</strong> quickly, ready to dazzle your audience. We guarantee opportune conveyance with the goal that you can start leveraging your custom merchandise at once." },
+]
